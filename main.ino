018fd00d6f1a7f5edb725ab520dbf171721f3a26
Steve Tchana <135659522+sttcha31@users.noreply.github.com>
2025-05-23 23:27:01 -0700

    main

---
+++ main.ino
@@ -0,0 +1,51 @@
+#include "Arduino_BMI270_BMM150.h"
+
+struct Direction { // Always Normalize to be 1
+  float x;
+  float y;
+  float z;
+};
+
+struct Vector {
+  Direction direction;
+  float maginture;
+};
+
+float get_magnitude(float x, float y, float z);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Serial Started");
+
+  if(!IMU.begin()){ 
+    Serial.println("Failed to Start IMU");
+    while(1);
+  }
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in g's");
+  Serial.println("X\tY\tZ");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float x, y, z;
+  Direction DownDirection = Direction{0,0,1};
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+    if(abs(get_magnitude(x,y,z)-1) < 0.2){
+      DownDirection = {x/get_magnitude(x,y,z), y/get_magnitude(x,y,z), z/get_magnitude(x,y,z)};
+    }
+
+  }
+}
+
+inline float get_magnitude(float x, float y, float z){
+  return sqrt(x*x + y*y + z*z);
+}
+
